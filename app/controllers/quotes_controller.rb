eecacef2023f4f755a6cd3c8559e08f760087e80
Cory Selvidge <healincheese@gmail.com>
2019-05-12 15:28:25 +0000

    Modal button works correctly now.

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -5,9 +5,9 @@ class QuotesController < ApplicationController
 
   def create
     @quote = Quote.create(quote_params)
-      if @quote.invalid?
-        flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
-      end
+    if @quote.invalid?
+      flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
+    end
     redirect_to root_path
   end
 
